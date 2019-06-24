24ec8237ff30c22e3460dfe87546a08cb6abec54
pzaloznyi <pzaloznyi@gmail.com>
2019-06-24 09:45:41 +0300

    finish sorts

--- Target1/Properties/AssemblyInfo.cs
+++ Target1/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NAU_Practices")]
+[assembly: AssemblyTitle("Target1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NAU_Practices")]
+[assembly: AssemblyProduct("Target1")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
---
+++ Target2/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Target2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
